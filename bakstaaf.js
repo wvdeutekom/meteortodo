5b3876651282dc7bc9a184b6754d5a7d62b7028c
Wijnand van Deutekom <wijnand@zerok.nl>
2014-12-13 17:16:49 +0100

    Initial commit

---
+++ bakstaaf.js
@@ -0,0 +1,15 @@
+Tasks = new Mongo.Collection("tasks");
+
+if (Meteor.isClient) {
+  Template.body.helpers({
+    tasks: function() {
+      return Tasks.find({});
+      }
+  });
+}
+
+if (Meteor.isServer) {
+  Meteor.startup(function () {
+    // code to run on server at startup
+  });
+}
