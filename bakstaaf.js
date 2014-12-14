6174a87702ae57b082f7bd99b34ec7cf3ccdb493
Wijnand van Deutekom <wijnand@zerok.nl>
2014-12-15 00:25:27 +0100

    Created a todo list

--- bakstaaf.js
+++ bakstaaf.js
@@ -3,8 +3,29 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
   Template.body.helpers({
     tasks: function() {
-      return Tasks.find({});
-      }
+      return Tasks.find({}, {sort: {createdAt: -1}});
+    }
+    
+
+  });
+
+  Template.hello.greeting = "Hello world!";
+
+  Template.body.events({
+    "submit .new-task": function (event) {
+      var text = event.target.text.value;
+
+      Tasks.insert({
+        text: text,
+        createdAt: new Date()
+      });
+      
+      //Clear form
+      event.target.text.value = "";
+
+      //Prevent default form submit
+      return false;
+    }
   });
 }
 
